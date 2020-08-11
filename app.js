59c0fb5dfed65d1310351db20b586c79789d5c49
Joseph Steel <joseph.steel.iv@gmail.com>
2020-08-11 16:02:36 -0400

    Database testing

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'fsjstd-restapi.db'
+});
+
+// async IIFE to test database
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
