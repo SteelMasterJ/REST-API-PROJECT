dd679a96780184607750007170293f181de419ef
Joseph Steel <joseph.steel.iv@gmail.com>
2020-08-14 11:12:26 -0400

    Added GET courses route/ Models not syncing

--- routes.js
+++ routes.js
@@ -9,8 +9,9 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
 const auth = require('basic-auth');
-const { sequelize } = require('./models');
-const User = require('./models/user');
+const { User } = require('./models/user');
+const { Course } = require('./models/course');
+
 
 //async handler function
 function asyncHandler(cb){
@@ -23,7 +24,7 @@ function asyncHandler(cb){
     };
 }
 
-//Authentication code
+//Authentication function
 const authenticateUser = async (req, res, next) => {
     let message = null;
   
@@ -77,6 +78,10 @@ const authenticateUser = async (req, res, next) => {
     }
 };
 
+
+//*** USER ROUTES ***//
+
+
 //GET route that returns the authenticated user.
 router.get('/users', authenticateUser, asyncHandler( async (req, res) => {
     const authUser = req.currentUser;
@@ -85,5 +90,74 @@ router.get('/users', authenticateUser, asyncHandler( async (req, res) => {
         email: authUser.emailAddress
     });
 }));
+
+//POST route that creates a new user
+// Route that creates a new user.
+router.post('/users', [
+  check('name')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "name"'),
+  check('username')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "username"'),
+  check('password')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "password"'),
+], asyncHandler(async (req, res) => {
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
+
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
+
+    // Return the validation errors to the client.
+    return res.status(400).json({ errors: errorMessages });
+  }
+
+  // check if the user already exists, and if it does do not proceed
+  const newUser = req.body;
+  const allUsers = await User.findAll();
+  const existingUser = allUsers.find(u => u.emailAddress === newUser.emailAddress);
+  if (existingUser) {
+    errorMessage = "'Sorry, this user already exists'";
+    return res.status(400).json({ errors: errorMessage });
+  }
+
+  // Get the user from the request body.
+  const user = req.body;
+
+  // Hash the new user's password.
+  user.password = bcryptjs.hashSync(user.password);
+
+  // Add the user to the `users` array.
+  users.push(user);
+
+  // Set the status to 201 Created and end the response.
+  return res.status(201).end();
+}));
+
+
+
+//*** COURSE ROUTES ***//
+
+
+
+//GET route that returns a list of courses (including the user that owns each course)
+router.get('/courses', asyncHandler(async(req, res) => {
+  const courses = await Course.findAll({
+    include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: ["id", "firstName", "lastName", "emailAddress"]
+      },      
+    ],
+      attributes: ["id", "title", "description", "estimatedTime", "materialsNeeded", "userId"]
+  });
+    res.json(courses);
+}));
+
 
 module.exports = router;
